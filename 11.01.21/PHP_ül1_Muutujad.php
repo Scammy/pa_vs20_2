82dff0098cb68634795143441be217e15a231a0a
Scammy <teodor400@gmail.com>
2021-01-21 14:13:48 +0200

    PHP meeldetuletus

---
+++ 11.01.21/PHP_ül1_Muutujad.php
@@ -0,0 +1,33 @@
+<?php
+/* kommenteerimine
+    test
+PHP ül1 muutujad
+Teodor Leheste
+vs20
+ */
+//üks rida
+
+$nimi = "Talku";
+
+
+//muutujate loomine
+
+$enimi = "kpr";
+$pnimi = "Leheste";
+$kaal = "78";
+$vanus = "23";
+
+echo $enimi;
+echo $pnimi;
+echo $kaal;
+echo $vanus;
+
+$nimi = $enimi.' '.$pnimi;
+echo $nimi;
+
+echo 'Tere' .$nimi.'!<br>';
+
+
+
+
+?>
